179927a4526185423052677facf30b5bf6568a9c
ue05987 <mario.velasco@altia.es>
2020-09-22 18:13:45 +0200

    Spring boot proyect created

---
+++ src/main/java/com/proyect/putmeapp/PutmeappApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.putmeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PutmeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PutmeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyect/putmeapp/PutmeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.putmeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PutmeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
