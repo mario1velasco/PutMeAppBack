dee06341a5888c63e4a6eb255e6087737539efd3
ue05987 <mario.velasco@altia.es>
2020-10-31 12:06:51 +0100

    LOG Working, ApiError save all detailMessage to send to the front

--- src/main/java/com/putmeapp/error/ApiError.java
+++ src/main/java/com/putmeapp/error/ApiError.java
@@ -1,13 +1,16 @@
 package com.putmeapp.error;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 
 import lombok.Data;
+import lombok.extern.java.Log;
 
 @Data
+@Log
 public class ApiError {
     private HttpStatus status;
     private int statusVal;
@@ -23,7 +26,7 @@ public class ApiError {
         super();
         this.status = status;
         this.message = message;
-        this.errors = errors;
+        this.errors = this.errorMessageInfo(errors, exception);
         this.statusVal = statusVal;
     }
 
@@ -32,7 +35,7 @@ public class ApiError {
         this.status = status;
         this.statusVal = statusVal;
         this.message = message;
-        this.errors = Arrays.asList(error);
+        this.errors = this.errorMessageInfo(Arrays.asList(error), exception);
     }
 
     public ApiError(final HttpStatus status, final int statusVal, final String message, final List<String> errors,
@@ -40,7 +43,7 @@ public class ApiError {
         super();
         this.status = status;
         this.message = message;
-        this.errors = errors;
+        this.errors = this.errorMessageInfo(errors, exception);
         this.statusVal = statusVal;
         this.exception = exception;
     }
@@ -51,8 +54,26 @@ public class ApiError {
         this.status = status;
         this.statusVal = statusVal;
         this.message = message;
-        this.errors = Arrays.asList(error);
+        this.errors = this.errorMessageInfo(Arrays.asList(error), exception);
         this.exception = exception;
+    }
+
+    private List<String> errorMessageInfo(final List<String> errors, final Exception exception) {
+        log.info("Log Working -- errorMessageInfo");
+        List<String> errorsResult = this.findErrorMessagesInException(exception);
+        errorsResult.addAll(errors);
+        return errorsResult;
+    }
+
+    private List<String> findErrorMessagesInException(final Throwable throwable) {
+        List<String> errorsResult = new ArrayList<>();
+        if (throwable != null && throwable.getMessage() != null) {
+            errorsResult.add(throwable.getMessage());
+        }
+        if (throwable != null && throwable.getCause() != null) {
+            errorsResult.addAll(this.findErrorMessagesInException(throwable.getCause()));
+        }
+        return errorsResult;
     }
 
 }
